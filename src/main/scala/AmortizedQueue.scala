1b299696b6ba5be77f7e6634149b3aeabb584c86
Fred Blundun <frederick.blundun@gmail.com>
2015-05-16 18:45:35 +0100

    Added AmortizedQueue

---
+++ src/main/scala/AmortizedQueue.scala
@@ -0,0 +1,14 @@
+case class AmortizedQueue[+A](front: List[A], rear: List[A]) {
+
+  def head(): A = front.head
+
+  def tail(): AmortizedQueue[A] = front match {
+    case h :: Nil => AmortizedQueue(rear.reverse, Nil)
+    case h :: t => AmortizedQueue(t, rear)
+  }
+
+  def snoc[B >: A](b: B): AmortizedQueue[B] = front match {
+    case Nil => AmortizedQueue(List(b), Nil)
+    case _ => AmortizedQueue(front, b :: rear)
+  }
+}
